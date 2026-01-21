ea8833b7099ff92308e902524660ca280f3e4b1d
regis <gbrregispf@gmail.com>
2026-01-20 21:00:38 -0300

    update data

--- src/config.ts
+++ src/config.ts
@@ -1,23 +1,23 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  website: "",
   author: "Gabriel Regis",
-  profile: "https://satnaing.dev/",
+  profile: "https://gabrielregis.com",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
   title: "Blog",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: false,
   postPerIndex: 10,
   postPerPage: 10,
-  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+  scheduledPostMargin: 15 * 60 * 1000,
   showArchives: false,
-  showBackButton: true, // show back button in post detail
+  showBackButton: true,
   editPost: {
     enabled: false,
     text: "Edit page",
     url: "https://github.com/satnaing/astro-paper/edit/main/",
   },
   dynamicOgImage: true,
-  dir: "ltr", // "rtl" | "auto"
-  lang: "pt", // html lang code. Set this empty and default will be "en"
-  timezone: "America/Sao_Paulo", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  dir: "ltr", 
+  lang: "pt",
+  timezone: "America/Sao_Paulo",
 } as const;
